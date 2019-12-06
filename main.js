8bc2aed215044b220cd822e91d96f5fc892f77f2
Devin Ross <devin.ross0717@gmail.com>
2019-12-05 21:43:07 -0700

    Add part of replacing range with inputs - enabling submit button on inputs

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function enableClearFormButton() {
   var nameInput = document.getElementsByClassName("name");
   var guessInput = document.getElementsByClassName("guess");
   var clearFormButton = document.getElementById("clearForm");
-  if (nameInput.value === "") {
+  if (nameInput.value === "" ) {
       clearFormButton.disabled = true;
     } else {
       clearFormButton.disabled = false
@@ -19,3 +19,29 @@ document.getElementById("nameInputField2").value = "";
 document.getElementById("guessInputField").value = "";
 document.getElementById("nameInputField").value = "";
 }
+
+
+
+function submitRange() {
+  console.log('submit range');
+  var minInput = document.getElementById("minimum");
+  var maxInput = document.getElementById("maximum");
+  var submitRangeButton = document.getElementsByClassName("range-button");
+  if (minInput.value !== "" && maxInput.value !== "") {
+    submitRangeButton[0].classList.add("updateButton");
+    submitRangeButton[0].disabled = false;
+    }
+  else {
+    submitRangeButton[0].classList.remove("updateButton");
+    submitRangeButton[0].disabled = true
+
+  }
+
+}
+// function replaceCurrentRangeText() {
+//   console.log("replacing text");
+//   var minGuess = document.getElementById("min");
+//   var maxGuess = document.getElementById("max");
+//   console.log('variables');
+//   minGuess.replace(minGuess);
+// }
