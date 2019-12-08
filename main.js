8e5395e6103571486523c47bf093d4ba6fd3d7cd
Devin Ross <devin.ross0717@gmail.com>
2019-12-08 16:32:31 -0700

    Add functionality for validating the guesses to be within the range

--- main.js
+++ main.js
@@ -1,23 +1,21 @@
-var inputSubmit1 = document.querySelector(".input-submit1");
-var inputSubmit2 = document.querySelector(".input-submit2");
-var inputSubmit3 = document.querySelector(".input-submit3");
-var inputSubmit4 = document.querySelector(".input-submit4");
-var inputSubmitButton = document.querySelector(".submitGuessButton");
-
-inputSubmit1.addEventListener("input", enableSubmit);
-inputSubmit2.addEventListener("input", enableSubmit);
-inputSubmit3.addEventListener("input", enableSubmit);
-inputSubmit4.addEventListener("input", enableSubmit);
-
-
-
-function enableSubmit() {
-  if (inputSubmit1.value != '' && inputSubmit2.value != '' && inputSubmit3.value != '' && inputSubmit4.value != '') {
-    inputSubmitButton.disabled = false;
-  } else {
-    inputSubmitButton.disabled = true;
-  };
-};
+// var inputSubmit1 = document.querySelector(".input-submit1");
+// var inputSubmit2 = document.querySelector(".input-submit2");
+// var inputSubmit3 = document.querySelector(".input-submit3");
+// var inputSubmit4 = document.querySelector(".input-submit4");
+// var inputSubmitButton = document.querySelector(".submitGuessButton");
+//
+// inputSubmit1.addEventListener("input", enableSubmit);
+// inputSubmit2.addEventListener("input", enableSubmit);
+// inputSubmit3.addEventListener("input", enableSubmit);
+// inputSubmit4.addEventListener("input", enableSubmit);
+//
+// function enableSubmit() {
+//   if (inputSubmit1.value != '' && inputSubmit2.value != '' && inputSubmit3.value != '' && inputSubmit4.value != '') {
+//     inputSubmitButton.disabled = false;
+//   } else {
+//     inputSubmitButton.disabled = true;
+//   };
+// };
 
 function enableClearFormButton() {
   console.log("clear button enable");
@@ -39,9 +37,6 @@ document.getElementById("nameInputField2").value = "";
 document.getElementById("guessInputField").value = "";
 document.getElementById("nameInputField").value = "";
 }
-
-
-
 
 function submitRange() {
   console.log('submit range');
@@ -71,4 +66,18 @@ function replaceCurrentRangeText() {
 minRange.innerHTML = minGuess;
 maxRange.innerHTML = maxGuess;
 
+}
+function confirmGuessInput() {
+  var minGuess = document.getElementById("minOfRange").innerText;
+  var maxGuess = document.getElementById("maxOfRange").innerText;
+  var inputSubmitButton = document.getElementById("submitGuess");
+  var guessInput1 = parseInt(document.getElementById("guessInputField").value);
+  var guessInput2 = parseInt(document.getElementById("guessInputField2").value);
+  if (guessInput1 >= minGuess && guessInput1 <= maxGuess && guessInput2 >= minGuess && guessInput2 <= maxGuess){
+    inputSubmitButton.disabled = false;
+  }
+  else {
+    console.log("hey")
+    inputSubmitButton.disabled = true;
+  };
 }
